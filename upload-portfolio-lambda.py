f85ecf9610c8bcb62d35fd9cf1dc5ef6c9266a94
Mika Holmström <mika.j.holmstrom@gmail.com>
2019-07-30 16:13:49 +0300

    Fixed py script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,15 +2,20 @@ import boto3
 import StringIO
 import zipfile
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.whata.guru')
-build_bucket = s3.Bucket('portfoliobuild.whata.guru')
+    s3 = boto3.resource('s3')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.whata.guru')
+    build_bucket = s3.Bucket('portfoliobuild.whata.guru')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-     for nm in myzip.namelist():
-             obj = myzip.open(nm)
-             portfolio_bucket.upload_fileobj(obj, nm)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+         for nm in myzip.namelist():
+                 obj = myzip.open(nm)
+                 portfolio_bucket.upload_fileobj(obj, nm)
+
+
+    return 'Hello from lambda'
