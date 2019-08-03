d0d25ab2a56e234e2a157288df617bac2b0f2328
Mika Holmström <mika.j.holmstrom@gmail.com>
2019-08-03 14:26:31 +0300

    Added exceptio handling to build

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,28 +1,33 @@
 import boto3
 import StringIO
 import zipfile
-import datetime
+from datetime import datetime
 
 def lambda_handler(event, context):
-
-    s3 = boto3.resource('s3')
-
-    portfolio_bucket = s3.Bucket('portfolio.whata.guru')
-    build_bucket = s3.Bucket('portfoliobuild.whata.guru')
-
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-         for nm in myzip.namelist():
-                 obj = myzip.open(nm)
-                 portfolio_bucket.upload_fileobj(obj, nm)
-
-
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:eu-north-1:539076042504:deployPortfolioTopic')
-    now = datetime.now()
 
-    topic.publish(Subject="Portfolio deployed", Message="Portofolio deployed (%s)" % now)
+    try:
+        s3 = boto3.resource('s3')
+
+        portfolio_bucket = s3.Bucket('portfolio.whata.guru')
+        build_bucket = s3.Bucket('portfoliobuild.whata.guru')
+
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+             for nm in myzip.namelist():
+                     obj = myzip.open(nm)
+                     portfolio_bucket.upload_fileobj(obj, nm)
+
+        print "Job done!"
+        now = datetime.now()
+        topic.publish(Subject="Portfolio deployed", Message="Portofolio deployed (%s)" % now)
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Not deployed successfully")
+        raise
+
+
 
     return 'Hello from lambda'
